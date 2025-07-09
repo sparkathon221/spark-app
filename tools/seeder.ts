6a909427a8f63527afd4721ac16b70049819c8e7
Arnob-B <arnobbhakta00@gmail.com>
2025-07-09 11:36:18 +0530

    seeder added

---
+++ tools/seeder.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import products from "../assets/db/dataset.json";
+export interface Product {
+	product_id: string;
+	name: string;
+	main_category: string;
+	sub_category: string;
+	immage: string;
+	link: string;
+	ratings: string;
+	no_of_ratings: string;
+	discount_price: string;
+	actual_price: string;
+}
+const main = async () => {
+	const client = new PrismaClient();
+	const productDetails = products as Product[];
+	console.log("started populating")
+	await client.product.createMany({
+		data: productDetails
+	});
+	console.log("population done")
+}
+
+
+main().catch((e) => {
+	console.log("error " + e)
+})
